b4f23c79f290e7e956cd8e05cbebe2ea272f1dad
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-03-17 18:34:25 -0400

    Move SpaceshipGenerator and dependencies

---
+++ src/CentralCasting/SpaceshipGenerator.js
@@ -0,0 +1,199 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var random_service_1 = require("../generators/random-service");
+// 866: Spacecraft
+var SpaceshipGenerator = /** @class */ (function () {
+    function SpaceshipGenerator(randomService) {
+        var _this = this;
+        this.randomService = randomService;
+        // prettier-ignore
+        this.spaceshipTypeGenerator = new BaseGenerator(this.randomService, [
+            { weight: 3, generate: function () { return ({ name: 'Small fighter craft', description: 'One or two pilots are all this small military ship can carry.' }); } },
+            { weight: 3, generate: function () { return ({ name: 'Scout ship', description: 'A small maneuverable craft capable of traversing space and exploring a planet.' }); } },
+            { weight: 2, generate: function () { return ({ name: 'Small yacht', description: 'A small personal space craft.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Large yacht', description: 'A large private passenger craft.' }); } },
+            { weight: 4, generate: function () { return ({ name: 'Small freighter', description: 'Designed to carry small amounts of goods.' }); } },
+            { weight: 2, generate: function () { return ({ name: 'Large freighter', description: 'Designed to haul large quantities of goods.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Factory ship', description: 'A large, ungainly craft designed to process raw materials into usable form. Usually they are heavily automated with minimal crew.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Destroyer', description: 'A small, but well-armed military ship.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Passenger liner', description: 'Large luxury ship.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Colony ship', description: 'Carries colonists to new planets.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Battlewagon', description: 'Large, heavily armed military ship.' }); } }
+        ]);
+        // prettier-ignore
+        this.spaceCapabilitiesGenerator = new BaseGenerator(this.randomService, [
+            { weight: 1, generate: function () { return ({ name: 'Orbital Only', description: 'No interplanetary capability.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'No hyperdrive', description: 'Travel to nearby planets & moons, inside the star system.' }); } },
+            { weight: 2, generate: function () { return ({ name: 'Basic hyperspace', description: "Travel to nearby systems" }); } },
+            { weight: 12, generate: function () { return ({ name: 'Hyperspace capable', description: 'Can travel to anyplace in the galaxy.' }); } }
+        ]);
+        // prettier-ignore
+        this.armamentGenerator = new BaseGenerator(this.randomService, [
+            { weight: 5, generate: function () { return ({ name: 'None', description: 'Ship carries no weapons.' }); } },
+            { weight: 6, generate: function () { return ({ name: 'Lightly-armed', description: "Carries a minimal of number of legal weaponry for defensive purposes. Doesn't belong in a fire-fight." }); } },
+            { weight: 6, generate: function () { return ({ name: 'Well-armed', description: 'Carries enough weaponry to make others think twice before attacking it. Has a fair offensive strike capability.' }); } },
+            { weight: 2, generate: function () { return ({ name: 'Heavily-armed', description: 'This is a battle ship, well protected and capable of massive amounts of offensive destruction.' }); } },
+            { weight: 1, generate: function (spaceship) {
+                    if (spaceship.type !== "Battlewagon") {
+                        return _this.armamentGenerator.generate(spaceship);
+                    }
+                    return ({ name: 'Planet-buster', description: 'The unbelievable firepower in this spacecraft could level a planet' });
+                } }
+        ]);
+        // prettier-ignore
+        this.specialFeaturesGenerator = new RerollGenerator(new BaseGenerator(this.randomService, [
+            { weight: 5, generate: function () { return ({ name: 'None', description: 'Nothing special.' }); } },
+            { weight: 2, generate: function () {
+                    // TODO: Develop a personality for it.
+                    // Roll 6 times on Table 312A: Personality Trait.
+                    // Check to select personality traits.
+                    // Decide whether it is "male" or "female"
+                    return { name: "Personalized ship's computer", description: '' };
+                } },
+            { weight: 1, generate: function () {
+                    // TODO: Select this item on Table 855: Techno-Wonders.
+                    return { name: 'Techno-wonder Installed', description: '' };
+                } },
+            { weight: 2, generate: function () { return ({ name: 'Large cargo area', description: 'The ship has extra cargo bay.' }); } },
+            { weight: 3, generate: function () { return ({ name: 'Advanced computer', description: 'The computers are one step better (smarter, faster, more programs) than those found on similar ships.' }); } },
+            { weight: 2, generate: function () { return ({ name: 'Special defenses', description: 'Defense systems are one step better than those found on similar ships.' }); } },
+            { weight: 2, generate: function () {
+                    // TODO: 
+                    // If PC: Ship requires no crew other than the character to operate.
+                    // If NPC: Ship requires no crew to operate.
+                    return ({ name: 'No Crew', description: 'Ship requires no crew to operate.' });
+                } },
+            { weight: 1, generate: function () { return ({ name: 'Non-standard hyperdrive', description: 'Ship uses less fuel and jumps farther and faster than similar ships.' }); } },
+            { weight: 2, generate: function () { return ({ name: 'Reroll', description: 'Reroll twice' }); } }
+        ]));
+        // prettier-ignore
+        this.liabilitiesGenerator = new RerollGenerator(new BaseGenerator(this.randomService, [
+            { weight: 3, generate: function () { return ({ name: 'None', description: 'Everything is fine.' }); } },
+            { weight: 3, generate: function () { return ({ name: 'Alien manufacture', description: "Ship is not built to character's racial standards. Seats are wrong, controls labels are illegible and so on." }); } },
+            { weight: 1, generate: function () { return ({ name: 'Clunky hyperspace', description: 'Drive may not always function when engaged. 75% chance of working.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Small cargo area', description: 'Has half the normal cargo space' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Interior unfinished', description: 'Walls lack paneling, floorsare raw metal, loose wiring hangs everywhere.' }); } },
+            { weight: 1, generate: function () {
+                    var extraConsumption = _this.randomService.getRandomInt(1, 100).value;
+                    return ({ name: 'Fuel eater', description: "Inefficient hyperdrive consumes " + extraConsumption + "% more fuel than a similar ships." });
+                } },
+            { weight: 1, generate: function () {
+                    // TODO: GM Only: See entry #866 on Table 967: GM's Specials
+                    return ({ name: 'TODO', description: '...' });
+                } },
+            { weight: 1, generate: function () {
+                    var amountOfShips = _this.randomService.getRandomInt(1, 6).value;
+                    return ({ name: 'Junker', description: "Ship is built out of salvage. At least " + amountOfShips + " different ships went into her construction." });
+                } },
+            { weight: 1, generate: function () { return ({ name: 'Old ship', description: "Ship was playing the spaceways when the character's granddad was a boy." }); } },
+            { weight: 1, generate: function () { return ({ name: 'Ancient ship', description: 'Ship is very old, possibly dating back to the beginning of star travel.' }); } },
+            { weight: 1, generate: function () {
+                    var extraCost = _this.randomService.getRandomInt(1, 100).value;
+                    return ({ name: 'Custom job', description: "Most systems are nonstandard. Repairs are " + extraCost + "% more costly than normal." });
+                } },
+            { weight: 1, generate: function () { return ({ name: 'Recognizable ship', description: 'The ship stands out from other ships. Even common folk know her by name.' }); } },
+            { weight: 1, generate: function () { return ({ name: 'Infested', description: 'The ship is overrun by parasites.' }); } },
+            { weight: 1, generate: function () {
+                    var extraCrew = _this.randomService.getRandomInt(2, 8).value * 50; // 50, 100, ..., 400%
+                    return ({ name: 'Large Crew', description: "Ship requires a large crew to run, at least " + extraCrew + "% more crew than a similar ship." });
+                } },
+            { weight: 2, generate: function () { return ({ name: 'Reroll', description: 'Reroll twice' }); } }
+        ]));
+    }
+    SpaceshipGenerator.prototype.generate = function () {
+        var ship = new Spaceship();
+        ship.type = this.spaceshipTypeGenerator.generate(ship);
+        ship.spaceCapabilities = this.spaceCapabilitiesGenerator.generate(ship);
+        ship.armament = this.armamentGenerator.generate(ship);
+        ship.specialFeatures = this.specialFeaturesGenerator.generate(ship);
+        ship.liabilities = this.liabilitiesGenerator.generate(ship);
+        return ship;
+    };
+    return SpaceshipGenerator;
+}());
+exports.SpaceshipGenerator = SpaceshipGenerator;
+var Spaceship = /** @class */ (function () {
+    function Spaceship() {
+    }
+    return Spaceship;
+}());
+exports.Spaceship = Spaceship;
+var BaseGenerator = /** @class */ (function () {
+    function BaseGenerator(randomService, elements) {
+        var _this = this;
+        this.randomService = randomService;
+        this.elements = new Array();
+        elements.forEach(function (element) {
+            for (var i = 0; i < element.weight; i++) {
+                _this.elements.push(element);
+            }
+        });
+    }
+    BaseGenerator.prototype.generate = function (spaceship) {
+        var result = this.randomService.pickOne(this.elements).value;
+        return result.generate(spaceship);
+    };
+    return BaseGenerator;
+}());
+var RerollGenerator = /** @class */ (function () {
+    function RerollGenerator(generator) {
+        this.generator = generator;
+    }
+    RerollGenerator.prototype.generate = function (spaceship) {
+        var result = new Array();
+        var element = this.generator.generate(spaceship);
+        if (element.name == 'Reroll') {
+            var result1 = this.generate(spaceship);
+            var result2 = this.generate(spaceship);
+            result.push.apply(result, result1);
+            result.push.apply(result, result2);
+        }
+        else {
+            result.push(element);
+        }
+        return result;
+    };
+    return RerollGenerator;
+}());
+exports.RerollGenerator = RerollGenerator;
+var AlignmentAndAttitude = /** @class */ (function () {
+    function AlignmentAndAttitude() {
+    }
+    return AlignmentAndAttitude;
+}());
+var CentralCastingHeroesForTomorrowHub = /** @class */ (function () {
+    /**
+     * Create a Central Casting Heroes For Tomorrow Service Hub
+     */
+    function CentralCastingHeroesForTomorrowHub(_spaceship) {
+        this._spaceship = _spaceship;
+    }
+    Object.defineProperty(CentralCastingHeroesForTomorrowHub.prototype, "spaceship", {
+        /**
+         * Gets the 866 Spacecraft generator.
+         *
+         * @readonly
+         * @type {SpaceshipGenerator}
+         * @memberof CentralCastingHeroesForTomorrowHub
+         */
+        get: function () {
+            return this._spaceship;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return CentralCastingHeroesForTomorrowHub;
+}());
+exports.CentralCastingHeroesForTomorrowHub = CentralCastingHeroesForTomorrowHub;
+var CentralCastingFactory = /** @class */ (function () {
+    function CentralCastingFactory(randomService) {
+        if (randomService === void 0) { randomService = new random_service_1.RandomService(); }
+        this.randomService = randomService;
+    }
+    CentralCastingFactory.prototype.createHeroesForTomorrow = function () {
+        return new CentralCastingHeroesForTomorrowHub(new SpaceshipGenerator(this.randomService));
+    };
+    return CentralCastingFactory;
+}());
+exports.CentralCastingFactory = CentralCastingFactory;
+//# sourceMappingURL=SpaceshipGenerator.js.map
---
+++ src/CentralCasting/SpaceshipGenerator.ts
@@ -0,0 +1,203 @@
+import { RandomService } from '../generators/random-service';
+
+// 866: Spacecraft
+export class SpaceshipGenerator {
+    private spaceshipTypeGenerator: BaseGenerator<RandomElement>;
+    private spaceCapabilitiesGenerator: BaseGenerator<RandomElement>;
+    private armamentGenerator: BaseGenerator<RandomElement>;
+    private specialFeaturesGenerator: RerollGenerator;
+    private liabilitiesGenerator: RerollGenerator;
+    constructor(private randomService: RandomService) {
+        // prettier-ignore
+        this.spaceshipTypeGenerator = new BaseGenerator<RandomElement>(this.randomService, [
+            { weight: 3, generate: () => ({ name: 'Small fighter craft', description: 'One or two pilots are all this small military ship can carry.' }) },
+            { weight: 3, generate: () => ({ name: 'Scout ship', description: 'A small maneuverable craft capable of traversing space and exploring a planet.' }) },
+            { weight: 2, generate: () => ({ name: 'Small yacht', description: 'A small personal space craft.' }) },
+            { weight: 1, generate: () => ({ name: 'Large yacht', description: 'A large private passenger craft.' }) },
+            { weight: 4, generate: () => ({ name: 'Small freighter', description: 'Designed to carry small amounts of goods.' }) },
+            { weight: 2, generate: () => ({ name: 'Large freighter', description: 'Designed to haul large quantities of goods.' }) },
+            { weight: 1, generate: () => ({ name: 'Factory ship', description: 'A large, ungainly craft designed to process raw materials into usable form. Usually they are heavily automated with minimal crew.' }) },
+            { weight: 1, generate: () => ({ name: 'Destroyer', description: 'A small, but well-armed military ship.' }) },
+            { weight: 1, generate: () => ({ name: 'Passenger liner', description: 'Large luxury ship.' }) },
+            { weight: 1, generate: () => ({ name: 'Colony ship', description: 'Carries colonists to new planets.' }) },
+            { weight: 1, generate: () => ({ name: 'Battlewagon', description: 'Large, heavily armed military ship.' }) }
+        ]);
+        // prettier-ignore
+        this.spaceCapabilitiesGenerator = new BaseGenerator<RandomElement>(this.randomService, [
+            { weight: 1, generate: () => ({ name: 'Orbital Only', description: 'No interplanetary capability.' }) },
+            { weight: 1, generate: () => ({ name: 'No hyperdrive', description: 'Travel to nearby planets & moons, inside the star system.' }) },
+            { weight: 2, generate: () => ({ name: 'Basic hyperspace', description: "Travel to nearby systems" }) },
+            { weight: 12, generate: () => ({ name: 'Hyperspace capable', description: 'Can travel to anyplace in the galaxy.' }) }
+        ]);
+        // prettier-ignore
+        this.armamentGenerator = new BaseGenerator<RandomElement>(this.randomService, [
+            { weight: 5, generate: () => ({ name: 'None', description: 'Ship carries no weapons.' }) },
+            { weight: 6, generate: () => ({ name: 'Lightly-armed', description: "Carries a minimal of number of legal weaponry for defensive purposes. Doesn't belong in a fire-fight." }) },
+            { weight: 6, generate: () => ({ name: 'Well-armed', description: 'Carries enough weaponry to make others think twice before attacking it. Has a fair offensive strike capability.' }) },
+            { weight: 2, generate: () => ({ name: 'Heavily-armed', description: 'This is a battle ship, well protected and capable of massive amounts of offensive destruction.' }) },
+            { weight: 1, generate: (spaceship) => {
+                if (spaceship.type !== "Battlewagon"){
+                    return this.armamentGenerator.generate(spaceship);
+                }
+                return ({ name: 'Planet-buster', description: 'The unbelievable firepower in this spacecraft could level a planet' });
+            } }
+        ]);
+        // prettier-ignore
+        this.specialFeaturesGenerator = new RerollGenerator(
+            new BaseGenerator<RandomElement>(this.randomService, [
+                { weight: 5, generate: () => ({ name: 'None', description: 'Nothing special.' }) },
+                { weight: 2, generate: () => {
+                        // TODO: Develop a personality for it.
+                        // Roll 6 times on Table 312A: Personality Trait.
+                        // Check to select personality traits.
+                        // Decide whether it is "male" or "female"
+                        return { name: "Personalized ship's computer", description: '' };
+                } },
+                { weight: 1, generate: () => {
+                        // TODO: Select this item on Table 855: Techno-Wonders.
+                        return { name: 'Techno-wonder Installed', description: '' };
+                } },
+                { weight: 2, generate: () => ({ name: 'Large cargo area', description: 'The ship has extra cargo bay.' }) },
+                { weight: 3, generate: () => ({ name: 'Advanced computer', description: 'The computers are one step better (smarter, faster, more programs) than those found on similar ships.' }) },
+                { weight: 2, generate: () => ({ name: 'Special defenses', description: 'Defense systems are one step better than those found on similar ships.' }) },
+                { weight: 2, generate: () => {
+                    // TODO: 
+                    // If PC: Ship requires no crew other than the character to operate.
+                    // If NPC: Ship requires no crew to operate.
+                    return ({ name: 'No Crew', description: 'Ship requires no crew to operate.' });
+                } },
+                { weight: 1, generate: () => ({ name: 'Non-standard hyperdrive', description: 'Ship uses less fuel and jumps farther and faster than similar ships.' }) },
+                { weight: 2, generate: () => ({ name: 'Reroll', description: 'Reroll twice' }) }
+            ])
+        );
+        // prettier-ignore
+        this.liabilitiesGenerator = new RerollGenerator(
+            new BaseGenerator<RandomElement>(this.randomService, [
+                { weight: 3, generate: () => ({ name: 'None', description: 'Everything is fine.' }) },
+                { weight: 3, generate: () => ({ name: 'Alien manufacture', description: "Ship is not built to character's racial standards. Seats are wrong, controls labels are illegible and so on." }) },
+                { weight: 1, generate: () => ({ name: 'Clunky hyperspace', description: 'Drive may not always function when engaged. 75% chance of working.' }) },
+                { weight: 1, generate: () => ({ name: 'Small cargo area', description: 'Has half the normal cargo space' }) },
+                { weight: 1, generate: () => ({ name: 'Interior unfinished', description: 'Walls lack paneling, floorsare raw metal, loose wiring hangs everywhere.' }) },
+                { weight: 1, generate: () => {
+                    const extraConsumption = this.randomService.getRandomInt(1, 100).value;
+                    return ({ name: 'Fuel eater', description: `Inefficient hyperdrive consumes ${extraConsumption}% more fuel than a similar ships.` });
+                } },
+                { weight: 1, generate: () => {
+                    // TODO: GM Only: See entry #866 on Table 967: GM's Specials
+                    return ({ name: 'TODO', description: '...' });
+                } },
+                { weight: 1, generate: () => {
+                    const amountOfShips = this.randomService.getRandomInt(1, 6).value;
+                    return ({ name: 'Junker', description: `Ship is built out of salvage. At least ${amountOfShips} different ships went into her construction.` });
+                } },
+                { weight: 1, generate: () => ({ name: 'Old ship', description: "Ship was playing the spaceways when the character's granddad was a boy." }) },
+                { weight: 1, generate: () => ({ name: 'Ancient ship', description: 'Ship is very old, possibly dating back to the beginning of star travel.' }) },
+                { weight: 1, generate: () => {
+                    const extraCost = this.randomService.getRandomInt(1, 100).value;
+                    return ({ name: 'Custom job', description: `Most systems are nonstandard. Repairs are ${extraCost}% more costly than normal.` });
+                } },
+                { weight: 1, generate: () => ({ name: 'Recognizable ship', description: 'The ship stands out from other ships. Even common folk know her by name.' }) },
+                { weight: 1, generate: () => ({ name: 'Infested', description: 'The ship is overrun by parasites.' }) },
+                { weight: 1, generate: () => {
+                    const extraCrew = this.randomService.getRandomInt(2, 8).value * 50; // 50, 100, ..., 400%
+                    return ({ name: 'Large Crew', description: `Ship requires a large crew to run, at least ${extraCrew}% more crew than a similar ship.` });
+                } },
+                { weight: 2, generate: () => ({ name: 'Reroll', description: 'Reroll twice' }) }
+            ])
+        );
+    }
+    public generate(): Spaceship {
+        var ship = new Spaceship();
+        ship.type = this.spaceshipTypeGenerator.generate(ship);
+        ship.spaceCapabilities = this.spaceCapabilitiesGenerator.generate(ship);
+        ship.armament = this.armamentGenerator.generate(ship);
+        ship.specialFeatures = this.specialFeaturesGenerator.generate(ship);
+        ship.liabilities = this.liabilitiesGenerator.generate(ship);
+        return ship;
+    }
+}
+
+export class Spaceship {
+    type: SpaceshipType;
+    spaceCapabilities: DescriptiveName;
+    armament: DescriptiveName;
+    specialFeatures: DescriptiveName[];
+    liabilities: DescriptiveName[];
+}
+
+export interface DescriptiveName {
+    name: string;
+    description: string;
+}
+
+export interface SpaceshipType {}
+
+export interface RandomElement extends Generator<DescriptiveName> {
+    weight: number;
+}
+
+interface Generator<T extends DescriptiveName> {
+    generate(spaceship: Spaceship): T;
+}
+
+class BaseGenerator<TInput extends RandomElement> implements Generator<DescriptiveName> {
+    protected elements: TInput[];
+
+    constructor(protected randomService: RandomService, elements: TInput[]) {
+        this.elements = new Array<TInput>();
+        elements.forEach(element => {
+            for (let i = 0; i < element.weight; i++) {
+                this.elements.push(element);
+            }
+        });
+    }
+    generate(spaceship: Spaceship): DescriptiveName {
+        const result = this.randomService.pickOne(this.elements).value;
+        return result.generate(spaceship);
+    }
+}
+
+export class RerollGenerator {
+    constructor(private generator: BaseGenerator<RandomElement>) {}
+    generate(spaceship: Spaceship): DescriptiveName[] {
+        var result = new Array<DescriptiveName>();
+        const element = this.generator.generate(spaceship);
+        if (element.name == 'Reroll') {
+            var result1 = this.generate(spaceship);
+            var result2 = this.generate(spaceship);
+            result.push(...result1);
+            result.push(...result2);
+        } else {
+            result.push(element);
+        }
+        return result;
+    }
+}
+
+class AlignmentAndAttitude {}
+
+export class CentralCastingHeroesForTomorrowHub {
+    /**
+     * Create a Central Casting Heroes For Tomorrow Service Hub
+     */
+    constructor(private _spaceship: SpaceshipGenerator) {}
+
+    /**
+     * Gets the 866 Spacecraft generator.
+     *
+     * @readonly
+     * @type {SpaceshipGenerator}
+     * @memberof CentralCastingHeroesForTomorrowHub
+     */
+    public get spaceship(): SpaceshipGenerator {
+        return this._spaceship;
+    }
+}
+
+export class CentralCastingFactory {
+    constructor(private randomService: RandomService = new RandomService()) {}
+
+    public createHeroesForTomorrow(): CentralCastingHeroesForTomorrowHub {
+        return new CentralCastingHeroesForTomorrowHub(new SpaceshipGenerator(this.randomService));
+    }
+}
